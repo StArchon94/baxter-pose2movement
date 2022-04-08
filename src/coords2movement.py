f1f6d19477eff483de0a2d1755acdd7cb9ccc40c
Shengjie Lin <StArchon@outlook.com>
2022-04-08 15:14:33 -0500

    reset when idle; fix angles

--- src/coords2movement.py
+++ src/coords2movement.py
@@ -19,14 +19,16 @@ def _threaded_execution(*fs):
 
 
 class Coords2Movement:
-    TIMEOUT = 0.5
+    TIMEOUT = 0.3
     THRESHOLD = 0.002
+    RESET_TIMEOUT = 3
 
     def __init__(self) -> None:
         rospy.init_node('coords2movement', anonymous=True)
         rospy.Subscriber('/robot_poses/all', Float64MultiArray, callback=self.coords_callback, queue_size=1)
         self.baxter = Baxter()
         self.angles = None
+        self.last_time = None
         self._semaphore = BoundedSemaphore(1)
         rospy.loginfo('Coords2Movement Node is Up!')
         self.initialize()
@@ -35,6 +37,7 @@ class Coords2Movement:
 
     def initialize(self):
         self.enable()
+        print('moving to neutral pose')
         self.move_to_neutral()
 
         # This executes EndEffectorCommand.CMD_CALIBRATE command
@@ -89,7 +92,12 @@ class Coords2Movement:
         while not rospy.is_shutdown():
             if self.angles is None:
                 continue
-            self._move_arms_to_positions(*self.angles)
+            t = rospy.Time.now()
+            if (t - self.last_time).to_sec() > self.RESET_TIMEOUT:
+                self.move_to_neutral()
+                self.angles = None
+            else:
+                self._move_arms_to_positions(*self.angles)
 
     def compute_angles(self, coords):
         l01 = coords[1] - coords[0]
@@ -97,16 +105,22 @@ class Coords2Movement:
         l24 = coords[4] - coords[2]
         l13 = coords[3] - coords[1]
         l35 = coords[5] - coords[3]
-        s0_r = np.pi - np.arccos(np.dot(l01[:2], l02[:2]) / (np.linalg.norm(l01[:2]) * np.linalg.norm(l02[:2])))
-        s1_r = np.arctan2(l02[2], np.linalg.norm(l02[:2]))
-        e0_r = np.pi / 2 + np.arctan2(l24[2], np.linalg.norm(l24[:2]))
+
+        s0_r = np.sign(np.cross(-l01[:2], l02[:2])) * np.arccos(np.dot(-l01[:2], l02[:2]) / (np.linalg.norm(l01[:2]) * np.linalg.norm(l02[:2])))
+        s1_r = np.arctan2(-l02[2], np.linalg.norm(l02[:2]))
+        v1 = np.array([0, 0, -1]) + l02[2] / np.dot(l02, l02) * l02
+        v2 = l24 + np.dot(l02, -l24) / np.dot(l02, l02) * l02
+        e0_r = np.sign(np.dot(np.cross(v1, v2), l02)) * np.arccos(np.dot(v1, v2) / (np.linalg.norm(v1) * np.linalg.norm(v2)))
         e1_r = np.arccos(np.dot(l02, l24) / (np.linalg.norm(l02) * np.linalg.norm(l24)))
 
-        s0_l = np.pi - np.arccos(np.dot(-l01[:2], l13[:2]) / (np.linalg.norm(-l01[:2]) * np.linalg.norm(l13[:2])))
-        s1_l = np.arctan2(l13[2], np.linalg.norm(l13[:2]))
-        e0_l = -np.pi / 2 - np.arctan2(l35[2], np.linalg.norm(l35[:2]))
+        s0_l = np.sign(np.cross(l01[:2], l13[:2])) * np.arccos(np.dot(l01[:2], l13[:2]) / (np.linalg.norm(l01[:2]) * np.linalg.norm(l13[:2])))
+        s1_l = np.arctan2(-l13[2], np.linalg.norm(l13[:2]))
+        v1 = np.array([0, 0, -1]) + l13[2] / np.dot(l13, l13) * l13
+        v2 = l35 + np.dot(l13, -l35) / np.dot(l13, l13) * l13
+        e0_l = np.sign(np.dot(np.cross(v1, v2), l13)) * np.arccos(np.dot(v1, v2) / (np.linalg.norm(v1) * np.linalg.norm(v2)))
         e1_l = np.arccos(np.dot(l13, l35) / (np.linalg.norm(l13) * np.linalg.norm(l35)))
-        return [[0.5 - s0_l, -s1_l, e0_l, e1_l, 0, 0, 0], [-0.5 + s0_r, -s1_r, e0_r, e1_r, 0, 0, 0]]
+
+        return [[0.5 + s0_l, s1_l, e0_l, e1_l, 0, 0, 0], [-0.5 + s0_r, s1_r, e0_r, e1_r, 0, 0, 0]]
 
     def coords_callback(self, data):
         # 0 - left_shoulder
@@ -115,6 +129,7 @@ class Coords2Movement:
         # 3 - right_elbow
         # 4 - left_wrist
         # 5 - right_wrist
+        self.last_time = rospy.Time.now()
         coords = np_bridge.to_numpy_f64(data)
         self.angles = self.compute_angles(coords)
 
